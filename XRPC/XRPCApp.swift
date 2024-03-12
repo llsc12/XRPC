57ee4e9ae7da853163dd700d6d5c3fd5547ccbac
Lakhan Lothiyi <llsc12.dev@gmail.com>
2024-03-12 18:10:35 +0000

    Initial Commit

---
+++ XRPC/XRPCApp.swift
@@ -0,0 +1,17 @@
+//
+//  XRPCApp.swift
+//  XRPC
+//
+//  Created by Lakhan Lothiyi on 12/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct XRPCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
